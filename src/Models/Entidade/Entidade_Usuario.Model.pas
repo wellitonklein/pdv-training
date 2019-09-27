1dca76ae45664c3267f338656f952e79b950d9d6
Welliton Klein <welliton.fokushima@gmail.com>
2019-09-27 16:52:29 -0300

    Aula #198 Projeto #71 Modelando e Mapeando Usuarios

--- src/Models/Entidade/Entidade_Usuario.Model.pas
+++ src/Models/Entidade/Entidade_Usuario.Model.pas
@@ -3,49 +3,94 @@ unit Entidade_Usuario.Model;
 interface
 
 uses
-  Classes,
   DB,
+  Classes,
   SysUtils,
   Generics.Collections,
-  // ORMBr
-  ormbr.mapping.attributes,
-  ormbr.types.nullable,
+
+  /// orm
+  ormbr.types.blob,
+  ormbr.types.lazy,
   ormbr.types.mapping,
+  ormbr.types.nullable,
+  ormbr.mapping.classes,
   ormbr.mapping.register,
-  ormbr.types.blob;
+  ormbr.mapping.attributes;
 
 type
-
   [Entity]
   [Table('USUARIO', '')]
-  [PrimaryKey('ID', 'Chave primária')]
-  TUsuario = class
+  [PrimaryKey('GUUID', NotInc, NoSort, False, 'Chave primária')]
+  TUSUARIO = class
   private
-    { Private declaration }
-    FID: Integer;
-    FNome: string;
-    FSenha: string;
+    { Private declarations }
+    FGUUID: String;
+    FNOME: Nullable<String>;
+    FSENHA: Nullable<String>;
+    FTIPO: Nullable<Integer>;
+    FSTATUS: Nullable<Integer>;
+    FDATACADASTRO: TDateTime;
+    FDATAALTERACAO: TDateTime;
+    function GetGUUID: String;
+    function GetDATACADASTRO: TDateTime;
+    function GetDATAALTERACAO: TDateTime;
   public
-    { Public declaration }
-
-    [Restrictions([NoUpdate, NotNull])]
-    [Column('ID', ftInteger)]
-    [Dictionary('ID', 'Mensagem de validação', '', '', '', taCenter)]
-    property ID: Integer read FID write FID;
+    { Public declarations }
+    [Restrictions([NotNull])]
+    [Column('GUUID', ftString, 64)]
+    [Dictionary('GUUID', 'Mensagem de validação', '', '', '', taLeftJustify)]
+    property GUUID: String read GetGUUID write FGUUID;
 
     [Column('NOME', ftString, 60)]
     [Dictionary('Nome', 'Mensagem de validação', '', '', '', taLeftJustify)]
-    property NOME: string read FNome write FNome;
+    property NOME: Nullable<String> read FNOME write FNOME;
 
     [Column('SENHA', ftString, 20)]
     [Dictionary('Senha', 'Mensagem de validação', '', '', '', taLeftJustify)]
-    property SENHA: string read FSenha write FSenha;
-  end;
+    property SENHA: Nullable<String> read FSENHA write FSENHA;
 
+    [Column('TIPO', ftSmallint)]
+    [Dictionary('Tipo', 'Mensagem de validação', '', '', '', taCenter)]
+    property TIPO: Nullable<Integer> read FTIPO write FTIPO;
+
+    [Column('STATUS', ftSmallint)]
+    [Dictionary('Situação', 'Mensagem de validação', '', '', '', taCenter)]
+    property STATUS: Nullable<Integer> read FSTATUS write FSTATUS;
+
+    [Column('DATACADASTRO', ftDateTime)]
+    [Dictionary('Data de cadastro', 'Mensagem de validação', '', '', '', taCenter)]
+    property DATACADASTRO: TDateTime read GetDATACADASTRO write FDATACADASTRO;
+
+    [Column('DATAALTERACAO', ftDateTime)]
+    [Dictionary('Data de atualização', 'Mensagem de validação', '', '', '', taCenter)]
+    property DATAALTERACAO: TDateTime read GetDATAALTERACAO write FDATAALTERACAO;
+  end;
 
 implementation
 
+{ TUSUARIO }
+
+function TUSUARIO.GetDATAALTERACAO: TDateTime;
+begin
+  FDATAALTERACAO := Now;
+  Result := FDATAALTERACAO;
+end;
+
+function TUSUARIO.GetDATACADASTRO: TDateTime;
+begin
+  if (FDATACADASTRO = StrToDateTime('30/12/1899')) then
+    FDATACADASTRO := Now;
+  Result := FDATACADASTRO;
+end;
+
+function TUSUARIO.GetGUUID: String;
+begin
+  if (FGUUID.IsEmpty) then
+    FGUUID := TGUID.NewGuid.ToString;
+  Result := FGUUID;
+end;
+
 initialization
-  TRegisterClass.RegisterEntity(TUsuario);
+  TRegisterClass.RegisterEntity(TUSUARIO)
 
 end.
