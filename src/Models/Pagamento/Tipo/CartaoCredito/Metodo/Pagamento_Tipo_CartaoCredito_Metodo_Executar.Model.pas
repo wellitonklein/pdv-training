bab034b19a22f86acd3bed11efa2d9559d7cf23b
Welliton Klein <welliton.fokushima@gmail.com>
2019-10-01 14:21:35 -0300

    Aula #252 Projeto #123 Pagamentos no Proxy Fiscal

--- src/Models/Pagamento/Tipo/CartaoCredito/Metodo/Pagamento_Tipo_CartaoCredito_Metodo_Executar.Model.pas
+++ src/Models/Pagamento/Tipo/CartaoCredito/Metodo/Pagamento_Tipo_CartaoCredito_Metodo_Executar.Model.pas
@@ -11,6 +11,8 @@ type
     FParent: IPagamentoTipoMetodoModel;
     FVenda: IVendaModel;
     FValor: Currency;
+    procedure Gravar;
+    procedure Fiscal;
   public
     constructor Create(Parent: IPagamentoTipoMetodoModel; Venda: IVendaModel);
     destructor Destroy; override;
@@ -30,10 +32,25 @@ uses
 function TPagamentoTipoCartaoCreditoMetodoExecutarModel.&End: IPagamentoTipoMetodoModel;
 begin
   Result := FParent;
+  Gravar;
+  Fiscal;
+end;
 
+procedure TPagamentoTipoCartaoCreditoMetodoExecutarModel.Fiscal;
+begin
+  FVenda.ModalidadeFiscal.Proxy.Pagamento
+    .AddPagamento
+      .Tipo(Integer(tpCartaoCredito))
+      .Descricao('CARTAO DE CREDITO')
+      .Valor(FValor)
+    .&End.EndPagamento
+end;
+
+procedure TPagamentoTipoCartaoCreditoMetodoExecutarModel.Gravar;
+begin
   FParent.&End.Entidade(TPDVUpdatesModel.New.Entidade.VendaPagamentos);
   FParent.&End.Entidade.VENDA := FVenda.Entidade.GUUID;
-  FParent.&End.Entidade.TIPO  := Integer(tpCartaoCredito);
+  FParent.&End.Entidade.TIPO := Integer(tpCartaoCredito);
   FParent.&End.Entidade.VALOR := FValor;
   FParent.&End.DAO.Insert(FParent.&End.Entidade);
 end;
--- src/Models/Pagamento/Tipo/Dinheiro/Metodo/Pagamento_Tipo_Dinheiro_Metodo_Executar.Model.pas
+++ src/Models/Pagamento/Tipo/Dinheiro/Metodo/Pagamento_Tipo_Dinheiro_Metodo_Executar.Model.pas
@@ -11,6 +11,8 @@ type
     FParent: IPagamentoTipoMetodoModel;
     FVenda: IVendaModel;
     FValor: Currency;
+    procedure Gravar;
+    procedure Fiscal;
   public
     constructor Create(Parent: IPagamentoTipoMetodoModel; Venda: IVendaModel);
     destructor Destroy; override;
@@ -29,10 +31,25 @@ uses
 function TPagamentoTipoDinheiroMetodoExecutarModel.&End: IPagamentoTipoMetodoModel;
 begin
   Result := FParent;
+  Gravar;
+  Fiscal;
+end;
 
+procedure TPagamentoTipoDinheiroMetodoExecutarModel.Fiscal;
+begin
+  FVenda.ModalidadeFiscal.Proxy.Pagamento
+    .AddPagamento
+      .Tipo(Integer(tpDinheiro))
+      .Descricao('DINHEIRO')
+      .Valor(FValor)
+    .&End.EndPagamento
+end;
+
+procedure TPagamentoTipoDinheiroMetodoExecutarModel.Gravar;
+begin
   FParent.&End.Entidade(TPDVUpdatesModel.New.Entidade.VendaPagamentos);
   FParent.&End.Entidade.VENDA := FVenda.Entidade.GUUID;
-  FParent.&End.Entidade.TIPO  := Integer(tpDinheiro);
+  FParent.&End.Entidade.TIPO := Integer(tpDinheiro);
   FParent.&End.Entidade.VALOR := FValor;
   FParent.&End.DAO.Insert(FParent.&End.Entidade);
 end;
